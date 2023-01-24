94d123ca92bdd6a261e270d7f52e971489856878
Ludovic Muller <ludovicmuller1@gmail.com>
2023-01-24 19:11:28 +0100

    test: make sure that it always hits the endpoint if cache is not recheable

--- test/redis-cache.js
+++ test/redis-cache.js
@@ -12,24 +12,47 @@ describe('redis cache for sparql-proxy', () => {
   it('should not crash even if redis is not recheable', async () => {
     const app = express()
 
+    let counter = 0
+
+    nock('http://example.org')
+      .post('/query')
+      .times(3)
+      .reply(200, (_uri, _body) => {
+        return counter++
+      })
+
     app.use('/query', sparqlProxy({
-      endpointUrl: 'http://example.org/post-url-encoded/query',
+      endpointUrl: 'http://example.org/query',
       cache: {
         url: 'redis://user:password@unknown.redis.localhost:6380'
       }
     }))
 
-    nock('http://example.org').post('/post-url-encoded/query').reply(200, (uri, body) => body)
-
     const backupConsoleError = console.error
     console.error = (..._args) => { }
-    const res = await request(app)
+
+    const res1 = await request(app)
       .post('/query')
       .set('content-type', 'application/x-www-form-urlencoded')
       .send('query=' + encodeURIComponent(query))
       .expect(200)
+    assert.strictEqual(res1.text, '0')
+
+    const res2 = await request(app)
+      .post('/query')
+      .set('content-type', 'application/x-www-form-urlencoded')
+      .send('query=' + encodeURIComponent(query))
+      .expect(200)
+    assert.strictEqual(res2.text, '1')
+
+    const res3 = await request(app)
+      .post('/query')
+      .set('content-type', 'application/x-www-form-urlencoded')
+      .send('query=' + encodeURIComponent(query))
+      .expect(200)
+    assert.strictEqual(res3.text, '2')
+
     console.error = backupConsoleError
-    assert.strictEqual(res.text, query)
   })
 
   it('should always hit the endpoint (no cache configured)', async () => {
