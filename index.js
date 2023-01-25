f9fa8c79f9ea2709f4198e14e747947e0dee856e
Ludovic Muller <ludovicmuller1@gmail.com>
2023-01-25 13:02:11 +0100

    refactor: move cache-related functions in a dedicated file

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const debug = require('debug')
 const defaults = require('lodash/defaults')
 const fetch = require('node-fetch')
 const Router = require('express').Router
-const { createClient } = require('redis')
+const { sha1 } = require('./src/utils')
+const { getRedisClient, cacheKeyName, cacheResult } = require('./src/cache')
 const SparqlHttpClient = require('sparql-http-client')
-const { sha1, toBoolean } = require('./src/utils')
 SparqlHttpClient.fetch = fetch
 
 if (debug.enabled('trifid:*,')) {
@@ -38,80 +38,6 @@ const authBasicHeader = (user, password) => {
   return `Basic ${base64String}`
 }
 
-const getRedisClient = async (options) => {
-  const { url, ttl, clearAtStartup, disabled, prefix } = options
-  if (!url || toBoolean(disabled)) {
-    return null
-  }
-  logger(`Cache: enabled.`)
-
-  const cachePrefix = prefix || 'default'
-  const cacheTtl = ttl ? parseInt(`${ttl}`, 10) : 60 * 60
-
-  const client = createClient({ url })
-
-  // try the connection, and crash if it is not working
-  await client.connect()
-  await client.ping()
-
-  // remove all cache entries at startup if configured that way
-  if (toBoolean(clearAtStartup)) {
-    logger(`Cache: remove all entries in Redis that match the following pattern: '${cachePrefix}:*'…`)
-    for await (const key of client.scanIterator({
-      MATCH: `${cachePrefix}:*`
-    })) {
-      logger(`Cache: removing '${key}' entry…`)
-      await client.del(key)
-    }
-    logger(`Cache: removed all entries in Redis that match the following pattern: '${cachePrefix}:*'. Done!`)
-  }
-  await client.disconnect()
-
-  return {
-    client,
-    ttl: cacheTtl,
-    prefix: cachePrefix
-  }
-}
-
-/**
- * Generate a cache key using a prefix.
- *
- * @param {string} prefix The prefix to use for the cache key.
- * @param {string} name The name of the key to use for the cache.
- * @returns {string} The prefixed cache key.
- */
-const cacheKeyName = (prefix, name) => {
-  return `${prefix}:${name}`
-}
-
-/**
- * Store an entry in the cache.
- *
- * @param {import('@redis/client').RedisClientType} cacheClient Redis client.
- * @param {Response} result Response to store in the cache.
- * @param {string} cacheKey key for the cache entry.
- * @param {number} cacheTtl TTL for the cache entry.
- */
-const cacheResult = async (cacheClient, result, cacheKey, cacheTtl) => {
-  if (cacheClient && result.status < 400) {
-    const responseText = await result.text()
-    const responseHeaders = {}
-    result.headers.forEach((value, name) => {
-      responseHeaders[name] = value
-    })
-
-    await cacheClient.connect()
-    await cacheClient.set(cacheKey, JSON.stringify({
-      headers: responseHeaders,
-      status: result.status,
-      data: responseText
-    }))
-    await cacheClient.expire(cacheKey, cacheTtl)
-    await cacheClient.disconnect()
-  }
-}
-
 const sparqlProxy = (options) => {
   const queryOptions = {}
 
@@ -135,7 +61,7 @@ const sparqlProxy = (options) => {
   let cacheTtl = 0
   let cachePrefix = 'default'
   let cacheClient = null
-  const redisClientPromise = getRedisClient(options.cache || {}).catch((reason) => {
+  const redisClientPromise = getRedisClient(logger, options.cache || {}).catch((reason) => {
     console.error('ERROR[sparql-proxy/cache]: something went wrong while trying to init cache', reason)
   })
 
